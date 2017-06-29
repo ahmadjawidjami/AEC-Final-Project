07f709cadec0154bb913044170622457060e816e
pinair <riccardo.sibani@gmail.com>
2017-06-29 16:04:09 +0200

    Contract Fixed

--- app/migrations/2_deploy_contracts.js
+++ app/migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var Migrations = artifacts.require("Migrations");
 var Project = artifacts.require("Project");
-var Projects = artifacts.require("Projects");
+var ShareToken = artifacts.require("ShareToken");
 
 
 module.exports = function(deployer) {
@@ -8,7 +8,7 @@ module.exports = function(deployer) {
     deployer.deploy(Project).then(function(instance) {
         console.log(instance);
     });
-    deployer.deploy(Projects).then(function(instance) {
+    deployer.deploy(ShareToken).then(function(instance) {
         console.log(instance);
     });
 
