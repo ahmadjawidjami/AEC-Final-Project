f38b261abb8c6495e9e502a20c04a7220479236e
Ahmad Jawid Jami <ahmadjawid@Ahmads-MacBook-Pro.local>
2017-07-04 21:47:46 +0200

    @jawid, @akhlaqi, @rohullahayobi, @hekmatullah.sajid A draft of scheduler added

--- app/src/app.js
+++ app/src/app.js
@@ -75,3 +75,71 @@ app.listen(LOCAL_APP_PORT, () => {
     console.log("\n\n\n");
     console.log(`App started on port ${LOCAL_APP_PORT}`);
 });
+
+
+const Database = require("./database");
+const CreatorDB = new Database(require('../models/creators'));
+
+const interactor = require("./interaction")(web3);
+let killContract = function (data) {
+    interactor.showStatus(data).then(function (result) {
+        if (!result.goalReached){
+            //TODO remove the project from mongodb
+
+            interactor.kill(data);        }
+    })
+
+}
+
+CreatorDB
+    .getAll()
+    .then(result => {
+
+        let creators = result;
+        for (let i = 0; i< creators.length; i++){
+            let currentCreator = creators[i];
+
+            let creatorProjects = currentCreator.projects;
+           // console.log(creatorProjects[0].address)
+          //  console.log(creatorProjects)
+
+            for (let index = 0; index < creatorProjects.length; index++){
+
+                let data = {
+                    creator: currentCreator._id,
+                    project: creatorProjects[index].address
+                }
+
+              // //  console.log(data.project)
+              //
+              //  console.log(data.project)
+              //  interactor.showStatus(data).then(function (result) {
+                 //   if (!result.goalReached){
+                        console.log(creatorProjects[index])
+                        setTimeout(killContract, creatorProjects[index].deadline - Date.now(), data);
+                  //  }
+              //  })
+
+            }
+        }
+
+        // let promisesArray = [];
+        // Promise.all(promisesArray).then(result => {
+        // }).catch(error => console.log(error));
+    })
+    .catch(error => console.log(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
