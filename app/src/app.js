ce90b567e30d1cfe5ba3604e25afb6f30cfc426c
Ahmad Jawid Jami <ahmadjawid@Ahmads-MacBook-Pro.local>
2017-07-10 01:50:49 +0200

    testrpc now persists blockchain data. problem with restarting testrpc solved

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+
 // 3rd-party dependencies
 const express = require('express');
 const app = express();
@@ -15,18 +16,16 @@ const config = require("../resources/config");
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider());
 
+
 // Application config
 // Change it with the config file info
 const LOCAL_APP_PORT = 8080;
 const PUBLIC_APP_PORT = process.env.PUBLIC_APP_PORT || LOCAL_APP_PORT;
 
 // ====== uncomment this when you have docker set up
-const MONGO_HOST = process.env.MONGO_HOST;
-const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_HOST = process.env.MONGO_HOST || "localhost";
+const MONGO_PORT = process.env.MONGO_PORT || 27017;
 
-// ====== detete this when docker
-// const MONGO_HOST = "localhost";
-// const MONGO_PORT = 27017;
 
 // MongoDB connection
 const MONGO_URL = 'mongodb://' + MONGO_HOST + ':' + MONGO_PORT + '/dev';
@@ -47,20 +46,20 @@ app.use(express.static(__dirname + '/../client'));
 
 // check blockchain client
 // TODO: delete it when Docker is set up
-try {
-    if (!web3.net.listening)
-        throw false;
-
-} catch (error) {
-    console.log(`WARN: No blockchain client listening, started a ${config.client} client`);
-    console.log(path.resolve(__dirname, config.script));
-    exec(path.resolve(__dirname, config.script), function(error, stdout, stderr) {
-        console.log("TestRPC started");
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-    });
-}
+// try {
+//     if (!web3.net.listening)
+//         throw false;
+//
+// } catch (error) {
+//     console.log(`WARN: No blockchain client listening, started a ${config.client} client`);
+//     console.log(path.resolve(__dirname, config.script));
+//     exec(path.resolve(__dirname, config.script), function(error, stdout, stderr) {
+//         console.log("TestRPC started");
+//         if (error !== null) {
+//             console.log('exec error: ' + error);
+//         }
+//     });
+// }
 
 // import the router 
 require('./router.js')(app, web3);
@@ -80,61 +79,3 @@ app.listen(LOCAL_APP_PORT, () => {
     console.log("\n\n\n");
     console.log(`App started on port ${LOCAL_APP_PORT}`);
 });
-
-
-// const Database = require("./database");
-// const CreatorDB = new Database(require('../models/creators'));
-// const BackerDB = new Database(require('../models/backers'));
-
-// const interactor = require("./interaction")(web3);
-// let killContractIfFundingGoalNotReached = data => {
-//     interactor.showStatus(data).then(function(result) {
-//         if (!result.goalReached) {
-
-//             let query = { $pull: { "projects": { address: data.project } } }
-//                 //remove the project from array of creator projects
-//             CreatorDB.updatePull(query).then(result => {
-
-//                 console.log(result);
-
-//             }).catch(error => {
-//                 console.log(error);
-//             });
-
-//             //remove project reference to backers
-//             BackerDB.updatePull(query).then(result => {
-//                 console.log(result);
-
-//             }).catch(error => {
-//                 console.log(error)
-//             });
-
-//             interactor.kill(data);
-//         }
-//     })
-
-// }
-
-// CreatorDB
-//     .getAll()
-//     .then(result => {
-
-//         let creators = result;
-//         for (let i = 0; i < creators.length; i++) {
-//             let currentCreator = creators[i];
-
-//             let creatorProjects = currentCreator.projects;
-
-//             for (let index = 0; index < creatorProjects.length; index++) {
-
-//                 let data = {
-//                     creator: currentCreator._id,
-//                     project: creatorProjects[index].address
-//                 }
-
-//                 console.log(creatorProjects[index])
-//                 setTimeout(killContractIfFundingGoalNotReached, creatorProjects[index].deadline - Date.now(), data);
-//             }
-//         }
-//     })
-//     .catch(error => console.log(error));
